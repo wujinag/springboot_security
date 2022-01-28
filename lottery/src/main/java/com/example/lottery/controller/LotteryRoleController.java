546dcac2005bd0a9b7c565fde92c5f01f51b33b7
wujiang(0027021390) <0027021390>
2022-01-28 10:38:24 +0800

    spring security 角色权限

---
+++ lottery/src/main/java/com/example/lottery/controller/LotteryRoleController.java
@@ -0,0 +1,21 @@
+package com.example.lottery.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author wuj
+ * @since 2022-01-27
+ */
+@RestController
+@RequestMapping("/lottery-role")
+public class LotteryRoleController {
+
+}
+
---
+++ lottery/src/main/java/com/example/lottery/controller/LotteryUserController.java
@@ -0,0 +1,21 @@
+package com.example.lottery.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author wuj
+ * @since 2022-01-26
+ */
+@RestController
+@RequestMapping("/lottery-user")
+public class LotteryUserController {
+
+}
+
